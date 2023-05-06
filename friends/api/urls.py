cb4e97a9e5aa2bd03b4501b05fd2677e1256fd72
Daniil <sysoevdaniil@mail.ru>
2023-05-06 03:21:52 +0300

    bug fix and friendlist with status

--- friends/api/urls.py
+++ friends/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import UserViewSet, RequestsViewSet
+from .views import UserViewSet, RequestsViewSet, FriendsListViewSet
 from rest_framework import routers
 
 
 router = routers.SimpleRouter()
 router.register('users', UserViewSet)
 router.register('requests', RequestsViewSet)
+router.register('friends', FriendsListViewSet)
 
 
 urlpatterns = [
--- friends/api/views.py
+++ friends/api/views.py
@@ -13,22 +13,44 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = (permissions.AllowAny,)
+    allowed_methods = ('POST', 'GET', 'HEAD', 'OPTIONS')
 
     #создать пользователя и получить токен
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
+            print(1)
             FriendsList.objects.create(user=user)
+            print(2)
             token = Token.objects.create(user=user)
+            print(3)
             return Response({'token': token.key}, status=201)
         return Response(serializer.errors, status=400)
     
-    #получить всех пользователей
+    #получить всех пользователей или если авторизован всех кроме себя
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
+        if request.user.is_authenticated:
+            queryset = self.get_queryset().exclude(id=request.user.id)
+        else:
+            queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+    
+    #получить детально пользователя и добавить поле о статусе дружбы (нет ничего, есть исходящая заявка, есть входящая заявка, уже друзья)
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = UserSerializer(instance)
+        status = {"Пользователь": serializer.data, "Статус": "Ничего нет"}
+        if FriendsList.objects.get(user=instance).friends.filter(id=request.user.id).exists():
+            status['Статус'] = 'Вы уже друзья'
+        elif Requests.objects.filter(sender_id=instance, receiver_id=request.user.id).exists():
+            status['Статус'] = 'Вы отправили заявку'
+        elif Requests.objects.filter(sender_id=request.user.id, receiver_id=instance).exists():
+            status['Статус'] = 'Вам отправили заявку'
+        elif instance.id == request.user.id:
+            status['Статус'] = 'Это вы'
+        return Response(status, status=200)
     
     #логин и получение токена
     @action(methods=['post'], detail=False)
@@ -54,6 +76,19 @@ class UserViewSet(viewsets.ModelViewSet):
                 return Response({'error': 'Вы не можете отправить заявку сами себе'}, status=400)
             if Requests.objects.filter(sender_id=data['sender_id'], receiver_id=pk).exists():
                 return Response({'error': 'Такая заявка уже существует'}, status=400)
+            if FriendsList.objects.get(user=data['sender_id']).friends.filter(id=pk).exists() or FriendsList.objects.get(user=pk).friends.filter(id=data['sender_id']).exists():
+                return Response({'error': 'Вы уже друзья'}, status=400)
+            #если такая заявка существует от другого пользователя, то принять её
+            if Requests.objects.filter(sender_id=pk, receiver_id=data['sender_id']).exists():
+                instance = Requests.objects.get(sender_id=pk, receiver_id=data['sender_id'])
+                user1 = FriendsList.objects.get(user=instance.sender_id)
+                user1.friends.add(instance.receiver_id)
+                user1.save()
+                user2 = FriendsList.objects.get(user=instance.receiver_id)
+                user2.friends.add(instance.sender_id)
+                user2.save()
+                instance.delete()
+                return Response({'message': 'Заявка принята'}, status=201)
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
@@ -63,6 +98,7 @@ class RequestsViewSet(viewsets.ModelViewSet):
     queryset = Requests.objects.all()
     serializer_class = RequestsSerializer
     permission_classes = (permissions.IsAuthenticated,)
+    allowed_methods = ('POST', 'GET', 'HEAD', 'OPTIONS', 'DELETE')
 
     #создать заявку
     def create(self, request, *args, **kwargs):
@@ -106,7 +142,7 @@ class RequestsViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset()
         sended = queryset.filter(sender_id=request.user.id)
         serializer = self.get_serializer(sended, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=200)
     
     #посмотреть входящие заявки
     @action(methods=['get'], detail=False)
@@ -114,7 +150,7 @@ class RequestsViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset()
         received = queryset.filter(receiver_id=request.user.id)
         serializer = self.get_serializer(received, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=200)
     
     #принять заявку
     @action(methods=['post'], detail=True, url_path='accept')
@@ -137,5 +173,39 @@ class RequestsViewSet(viewsets.ModelViewSet):
         if request.user.is_staff:
             queryset = self.get_queryset()
             serializer = self.get_serializer(queryset, many=True)
-            return Response(serializer.data)
+            return Response(serializer.data, status=200)
         return Response({'error': 'У вас нет доступа к этому разделу'}, status=400)
+
+
+class FriendsListViewSet(viewsets.ModelViewSet):
+    queryset = FriendsList.objects.all()
+    serializer_class = FriendSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    allowed_methods = ('GET', 'HEAD', 'OPTIONS', 'DELETE')
+
+    #посмотреть список друзей
+    def list(self, request, *args, **kwargs):
+        friends = FriendsList.objects.get(user=request.user.id)
+        serializer = self.get_serializer(friends)
+        #возвращать username друзей
+        for i in range(len(serializer.data['friends'])):
+            serializer.data['friends'][i] = get_user_model().objects.get(id=serializer.data['friends'][i]).username
+        return Response(serializer.data['friends'], status=200)
+    
+    #удалить друга и переместить его в список заявок
+    def destroy(self, request, pk=None, *args, **kwargs):
+        queryset = self.get_queryset()
+        instance = queryset.get(user=request.user.id)
+        if instance.friends.filter(id=pk).exists():
+            #если друга не существует или его нет в списке друзей, то вернуть ошибку
+            if not get_user_model().objects.filter(id=pk).exists() and not instance.friends.filter(id=pk).exists():
+                return Response({'error': 'Такого друга нет'}, status=400)
+            instance.friends.remove(pk)
+            Requests.objects.create(sender_id=get_user_model().objects.get(id=pk), receiver_id=request.user)
+            #удалить из друзей друга, которого удалили
+            if FriendsList.objects.get(user=pk).friends.filter(id=request.user.id).exists():
+                user = FriendsList.objects.get(user=pk)
+                user.friends.remove(request.user.id)
+                user.save()
+            return Response({'message': 'Друг удалён'}, status=204)
+        return Response({'error': 'Такого друга нет'}, status=400)
